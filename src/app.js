a1e007cc525a038649724a53a31f204de5fa4b8a
lgmacedo <lucasguerra.macedo@gmail.com>
2023-05-16 15:06:15 -0300

    feat: Implement initial server architecture

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import router from "./routers/index.router.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(`Server running on port ${PORT}`)
+);

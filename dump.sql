4a9d276b0ecba5ad5577517ff2e3d514a53676f5
lgmacedo <lucasguerra.macedo@gmail.com>
2023-05-18 17:27:44 -0300

    fix: Minor database model change

--- dump.sql
+++ dump.sql
@@ -28,7 +28,7 @@ CREATE TABLE public.sessions (
     id integer NOT NULL,
     "userId" integer NOT NULL,
     token text NOT NULL,
-    "createdAt" date DEFAULT now() NOT NULL
+    "createdAt" timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 
@@ -62,7 +62,7 @@ CREATE TABLE public.urls (
     "shortUrl" text NOT NULL,
     "userId" integer NOT NULL,
     "visitCount" integer DEFAULT 0 NOT NULL,
-    "createdAt" date DEFAULT now() NOT NULL
+    "createdAt" timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 
@@ -95,7 +95,7 @@ CREATE TABLE public.users (
     name text NOT NULL,
     email text NOT NULL,
     password text NOT NULL,
-    "createdAt" date DEFAULT now() NOT NULL,
+    "createdAt" timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
     CONSTRAINT users_email_check CHECK ((email ~~ '%_@__%.__%'::text))
 );
 
@@ -145,54 +145,42 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 -- Data for Name: sessions; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.sessions VALUES (1, 1, '05d00ab9-e25d-48dd-9c79-09f84eee4f07', '2023-05-17');
-INSERT INTO public.sessions VALUES (2, 2, '0d573c98-7c86-493f-9e73-f8662b2b934a', '2023-05-17');
-INSERT INTO public.sessions VALUES (3, 3, '367daef0-8d00-41dc-bd30-16c77465e408', '2023-05-17');
-INSERT INTO public.sessions VALUES (4, 4, 'c1107c4a-303f-44f2-ba62-63df7179210f', '2023-05-18');
+INSERT INTO public.sessions VALUES (1, 1, '4ae5eacb-23b4-4406-a22f-219ebec59d29', '2023-05-18 17:23:38.66323');
 
 
 --
 -- Data for Name: urls; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.urls VALUES (4, 'https://www.youtube.com/watch?v=czTksCF6X8Y', '11GzKO_q', 2, 0, '2023-05-17');
-INSERT INTO public.urls VALUES (5, 'https://www.youtube.com/watch?v=czTksCF6X8Y', 'GEFGZ3Kd', 2, 0, '2023-05-17');
-INSERT INTO public.urls VALUES (6, 'https://www.youtube.com/watch?v=ARVKSdd8bxo', 'RNFwYZqc', 2, 0, '2023-05-17');
-INSERT INTO public.urls VALUES (7, 'https://www.youtube.com/watch?v=XTIOq_cFYns', 'xD1CdOnF', 3, 0, '2023-05-17');
-INSERT INTO public.urls VALUES (1, 'https://www.youtube.com/watch?v=Q4rEJr3sUO8', 'ZgMIol1s', 1, 4, '2023-05-17');
-INSERT INTO public.urls VALUES (3, 'https://www.youtube.com/watch?v=k7H2C5L8X7I', 'fmadclnD', 1, 1, '2023-05-17');
-INSERT INTO public.urls VALUES (8, 'https://www.youtube.com/watch?v=QH2-TGUlwu4', 'vXtDwwId', 4, 0, '2023-05-18');
+INSERT INTO public.urls VALUES (1, 'https://www.youtube.com/watch?v=QH2-TGUlwu4', '5xvtJJq4', 1, 0, '2023-05-18 17:25:00.089578');
 
 
 --
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.users VALUES (1, 'Lalo Salamanca', 'lalo@gmail.com', '$2b$10$dXdoXqlFviOVDbaMCnelg.ZYXVGVi8C9gXIstL3JvEqSDsqhcOAf6', '2023-05-17');
-INSERT INTO public.users VALUES (2, 'Lucas Macedo', 'lucas@gmail.com', '$2b$10$pcjf4M6mpvEczVtkpPkQzu8kWnTYLhPDBWDA4EKDMZnz9CLdcUGim', '2023-05-17');
-INSERT INTO public.users VALUES (3, 'Yasmin Haddad', 'yasmin@gmail.com', '$2b$10$FTX4p82PJh/VzMSWuSQX3uxgu5t3b2WnA4qrI/IK60jzjkCa4AMdm', '2023-05-17');
-INSERT INTO public.users VALUES (4, 'Toninho Rodrigues', 'trodrigues@gmail.com', '$2b$10$CvRNzGs783J4PHhTNVmEse6UquPMQ9Fudw1E1njcPep8Lx0w34xV2', '2023-05-18');
+INSERT INTO public.users VALUES (1, 'Lucas Macedo', 'lucas@gmail.com', '$2b$10$rpV/fPL4EqsQ0uB48ToK1OdmjE/l1X66NpdiHwo9CVNM5eEb5hQNm', '2023-05-18 17:23:22.828386');
 
 
 --
 -- Name: sessions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.sessions_id_seq', 4, true);
+SELECT pg_catalog.setval('public.sessions_id_seq', 1, true);
 
 
 --
 -- Name: urls_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.urls_id_seq', 8, true);
+SELECT pg_catalog.setval('public.urls_id_seq', 1, true);
 
 
 --
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.users_id_seq', 4, true);
+SELECT pg_catalog.setval('public.users_id_seq', 1, true);
 
 
 --
